940f8ea1b81f41ad8bb361ea9ef80318e4ee7832
Julien Calenge <julien.calenge@epitech.eu>
2021-02-13 15:48:29 +0100

    Removed useless requirements

--- sample/browser.py
+++ sample/browser.py
@@ -2,10 +2,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
-from flask import Flask, jsonify, json
-from flask_cors import CORS
-from flask import request
-
 import sys
 from subprocess import Popen
 
