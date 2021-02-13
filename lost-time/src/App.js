1e517a982aaef94447d3d1b278c0be90fbd38359
Halil Bagdadi <halil.bagdadi@epitech.eu>
2021-02-13 18:31:02 +0100

    adding date function + init image upload module

--- lost-time/src/App.js
+++ lost-time/src/App.js
@@ -2,19 +2,48 @@ import logo from './logo_lost-time.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import './App.css';
+import React from 'react';
+import ImageUploader from 'react-images-upload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="info">Tell us more about you</Button>{' '}
-      </header>
-      <footer>
-        <p>Made with love by <i>Halil BAGDADI</i>, <i>Julien CALENGE</i> et <i>Clement GERINIERE</i></p>
-      </footer>
-    </div>
-  );
+function date() {
+  var d = new Date();
+  var fullDate = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+  // console.log(fullDate);
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(picture) {
+    this.setState({
+      pictures: this.state.pictures.concat(picture),
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Button variant="info">Tell us more about you</Button>{' '}
+          <ImageUploader
+            withIcon={true}
+            buttonText='Choose images'
+            onChange={this.onDrop}
+            imgExtension={['.jpg', '.png', '.jpeg']}
+            maxFileSize={20242880}
+          />
+        </header>
+        <footer>
+          <p>Made with love by <i>Halil BAGDADI</i>, <i>Julien CALENGE</i> and <i>Clement GERINIERE</i></p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
